61dc20e5f51bdd5de6029d1a98f864f217a528d9
varnikak00 <91057679+varnikak00@users.noreply.github.com>
2021-10-19 13:53:08 +0530

    Update Contact.cs

--- Contact.cs
+++ Contact.cs
@@ -1,6 +1,23 @@
-﻿namespace AddressBook1
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
 {
-    internal class Contact
+    class Contact
     {
+        private string firstname, lastname, address, city, state, email;
+        private int zipNo;
+        private long Phonenumber;
+
+        public string Firstname { get { return firstname; } set { firstname = value; } }
+        public string Lastname { get { return lastname; } set{ lastname = value; } }
+        public string Address { get { return address; } set { address = value; } }
+        public string City { get { return city; } set { city = value; } }
+        public string State { get { return state; } set { state = value; } }
+        public string Email { get { return email; } set { email = value; } }
+        public int Zipno { get { return zipNo; } set { zipNo = value; } }
+        public long PhoneNo { get { return Phonenumber; } set {Phonenumber = value; }  }
+
     }
 }
