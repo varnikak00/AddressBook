17054fe83e06168d0a427f455d412bede3cfea6e
varnikak00 <varnikak7070@gmail.com>
2021-10-19 16:02:52 +0530

    welcome

---
+++ Contact.cs
@@ -0,0 +1,18 @@
+﻿namespace AddressBookIoFile
+{
+    internal class Contact
+    {
+        private string firstname, lastname, address, city, state, email;
+        private int zipNo;
+        private long Phonenumber;
+
+        public string Firstname { get { return firstname; } set { firstname = value; } }
+        public string Lastname { get { return lastname; } set { lastname = value; } }
+        public string Address { get { return address; } set { address = value; } }
+        public string City { get { return city; } set { city = value; } }
+        public string State { get { return state; } set { state = value; } }
+        public string Email { get { return email; } set { email = value; } }
+        public int Zipno { get { return zipNo; } set { zipNo = value; } }
+        public long PhoneNo { get { return Phonenumber; } set { Phonenumber = value; } }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AddressBookIoFile
+{
+    class Program
+    {
+        
+            static IDictionary<string, Contact> addressBook = new Dictionary<string, Contact>();
+
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Welcome to Address Book");
+
+                CRUDoperationOnContact crudContact = new CRUDoperationOnContact();
+
+                bool xyz = true;
+                while (xyz)
+                {
+                    Console.WriteLine("select option");
+                    Console.WriteLine("1. Add new contact \n2. Update contact \n3. Delete contact" +
+                        "\n4. Display All Record\n5. Display person based on state or City" +
+                        "\n6. Display person name and city\n7. Display person name and state" +
+                        "\n8. Read File using StreamReader\n9. Exit program");
+                    int option = int.Parse(Console.ReadLine());
+                    switch (option)
+                    {
+                        case 1:
+                            crudContact.takeInput(addressBook);
+                            break;
+                        case 2:
+                            crudContact.updateRecord(addressBook);
+                            break;
+                        case 3:
+                            crudContact.deleteRecord(addressBook);
+                            break;
+                        case 4:
+                            crudContact.displayAddressBook(addressBook);
+                            break;
+                        case 5:
+                            crudContact.DisplayPersonName(addressBook);
+                            break;
+                        case 6:
+                            crudContact.DisplayPersonAndCity();
+                            break;
+                        case 7:
+                            crudContact.DisplayPersonAndState();
+                            break;
+                        case 8:
+                            crudContact.ReadFileUsingStreamReader();
+                            break;
+                        case 9:
+                            return;
+                        default:
+                            xyz = false;
+                            break;
+                    }
+                }
+
+            }
+        }
+    }
